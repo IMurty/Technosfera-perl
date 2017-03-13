5d12bedf2cafb343daa95d1730ad49b80a4171bc
iMurty <igataullin.g4s@mail.ru>
2017-03-14 01:54:42 +0300

    anagrams hw

--- homeworks/anagrams/lib/Anagram.pm
+++ homeworks/anagrams/lib/Anagram.pm
@@ -3,7 +3,7 @@ package Anagram;
 use 5.010;
 use strict;
 use warnings;
-
+use Encode qw(decode encode);
 =encoding UTF8
 
 =head1 SYNOPSIS
@@ -42,12 +42,55 @@ anagram(['пятак', 'ЛиСток', 'пятка', 'стул', 'ПяТаК', '
 sub anagram {
     my $words_list = shift;
     my %result;
+    foreach my $word (@$words_list) {
+		$word = lc(decode('UTF8',$word));
+		$word = encode('UTF8', $word);
+		my $have_key = return_key (\%result, $word);
+    	if ($have_key) {
+			my $arr_ref = $result{$have_key};
+			unless (exists_in_arr($arr_ref,$word)) {
+				push @$arr_ref, $word;
+				@$arr_ref = sort @$arr_ref;
+				$result{$have_key} = $arr_ref;
+			}
+		}
+		else {
+			$result{$word} = [$word];
+		}
+	}
+	return rm_small_arrs(\%result);
+};
 
-    #
-    # Поиск анограмм
-    #
+sub return_key {
+	my ($hash_ref, $add_word) = @_;
+	foreach my $key (keys %$hash_ref) {
+    	if (str_sort($key) eq str_sort($add_word)) {
+			return $key;
+    	}
+	}
+	return undef;
+};
 
-    return \%result;
+sub str_sort {
+	my $word = shift;
+	my @word = split '', $word;
+	my $str = join '',sort @word;
+	return lc($str);
+};
+
+sub exists_in_arr {
+	my ($arr_ref, $word) = @_;
+	return scalar grep /^$word$/, @$arr_ref;
 }
 
+sub rm_small_arrs {
+	my $hash_ref = shift;
+	foreach my $key (keys %$hash_ref) {
+    	my $arr_ref = $hash_ref->{$key};
+    	if ((scalar @$arr_ref) < 2){
+        	delete $hash_ref->{$key};
+    	}
+	}
+	return $hash_ref;
+}
 1;
