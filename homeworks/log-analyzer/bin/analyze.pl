982d060785bddfee5cd7d5cb5f72361000fa66c7
iMurty <igataullin.g4s@mail.ru>
2017-03-17 21:06:14 +0300

    log-analyzer hw

--- homeworks/log-analyzer/bin/analyze.pl
+++ homeworks/log-analyzer/bin/analyze.pl
@@ -9,33 +9,72 @@ my $filepath = $ARGV[0];
 die "USAGE:\n$0 <log-file.bz2>\n"  unless $filepath;
 die "File '$filepath' not found\n" unless -f $filepath;
 
-my $parsed_data = parse_file($filepath);
-report($parsed_data);
+my @parsed_data = parse_file($filepath);
+report(@parsed_data);
 exit;
 
 sub parse_file {
     my $file = shift;
+    my %result;
+    my $ip_ptrn = '(?<ip>\d+\.\d+\.\d+\.\d+)';
+    my $date_ptrn = '\[(?<day>\d{2})\/(?<mounth>\w{3})\/(?<year>\d{4}):(?<hours>\d{2}):(?<minutes>\d{2}):(?<seconds>\d{2}) (?<timezone>\+\d{4})\]';
+    my $status_ptrn = '(?<status>\d{3})';
+    my $bytes_ptrn = '(?<bytes>\d+)';
+    my $coefficient_ptrn  = '"(?<coefficient>[^"]+?)"';
 
-    # you can put your code here
-
-    my $result;
     open my $fd, "-|", "bunzip2 < $file" or die "Can't open '$file': $!";
     while (my $log_line = <$fd>) {
-
-        # you can put your code here
-        # $log_line contains line from log file
-
+		#get ip
+        $log_line =~ /^$ip_ptrn/;
+		my $ip = $+{ip};
+        $result{$ip}->{count} += 1;
+        $result{total}->{count} += 1;
+		$' =~ /^\s+$date_ptrn/;
+		#get date
+		$result{total}{minutes}{$+{day}.$+{mounth}.$+{year}.$+{hours}.$+{minutes}} = 1;
+		$result{$ip}->{minutes}->{$+{day}.$+{mounth}.$+{year}.$+{hours}.$+{minutes}} = 1;
+		#get status and bytes
+        $' =~ /\s+$status_ptrn\s+$bytes_ptrn/;
+        my $status = $+{status}+0;
+        my $data   = $+{bytes}+0;
+        $result{$ip}->{status}->{$status} += $data;
+        $result{total}->{status}->{$status} += $data;
+		#get coefficient
+        $' =~ /$coefficient_ptrn$/;
+        my $zip_coeff = $+{coefficient} eq "-" ? 1 : $+{coefficient}+0;
+		#calculation the amount of data
+		if ( $status == 200 ) {
+            $result{$ip}->{data} += int( $zip_coeff * $data );
+        	$result{total}->{data} += int( $zip_coeff * $data );
+		}
     }
     close $fd;
-
-    # you can put your code here
-
-    return $result;
+	my @sorted_ip_list = sort {$result{$b}->{count} <=> $result{$a}->{count}} keys %result;
+	foreach (@sorted_ip_list[11..$#sorted_ip_list]) {
+		delete $result{$_};
+	}
+    for my $ip ( keys %result ) {
+        $result{$ip}->{average} = $result{$ip}->{count}/(scalar keys %{$result{$ip}->{minutes}});
+		delete $result{$ip}->{minutes};
+    }
+	@sorted_ip_list = @sorted_ip_list[0..10];
+	return (\%result, \@sorted_ip_list);
 }
 
 sub report {
-    my $result = shift;
-
-    # you can put your code here
-
+	my $result = shift;
+	my $list = shift;
+	my @statuses = sort {$a <=> $b} keys %{$result->{total}->{status}};
+	my $row_format = "%s\t%d\t%.2f\t%d".("\t%d" x @statuses)."\n";
+	my $header_format = "IP	count	avg	data".("\t%d" x @statuses)."\n";
+	my $statuses_str = join "\t",@statuses;
+	printf $header_format, @statuses;
+	foreach my $ip (@$list) {
+		my @str;
+		foreach (@statuses) {
+			my $push = exists ($result->{$ip}->{status}->{$_}) ? $result->{$ip}->{status}->{$_}/1024: 0;
+			push @str, $push;
+		}
+		printf $row_format, $ip, $result->{$ip}->{count}, $result->{$ip}->{average}, $result->{$ip}->{data}/1024, @str;
+	}
 }
