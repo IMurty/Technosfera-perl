b9e82c384d5483d95471941a0ead20f0021749cf
iMurty <igataullin.g4s@mail.ru>
2017-04-05 01:22:13 +0300

    myconst started

--- homeworks/myconst/lib/myconst.pm
+++ homeworks/myconst/lib/myconst.pm
@@ -37,6 +37,43 @@ print ZERO;             # 0
 print PI;               # 3.14
 =cut
 
+our %CONSTANTS;
+our %GROUPS;
+
+sub import {
+    my ( $self, @str ) = @_;
+	die "have not args\n" if @str == 1;
+	foreach (@str) {
+		die "INVALID ARGS!" if (!defined $_) ;
+	}
+	my %hash = @str;
+	my $caller = caller(0);
+	&parse_hash (\%hash);
+	no strict 'refs';
+    for my $key ( keys %CONSTANTS ) {
+        *{"$caller::$key"} = sub() { $CONSTANTS{$key}; };
+    };
+};
+
+sub parse_hash {
+    my $hash = shift;
+	my $group_name = shift;
+    while (my ($key, $value) = each %$hash) {
+		die if (ref $value eq 'ARRAY');
+        if ('HASH' eq ref $value) {
+			die "INVALID ARGS!" unless (%$value);
+            &parse_hash ($value, $key);
+        }
+        elsif ((defined $key) && (defined $value)) {
+            push @{$GROUPS{all}}, $key;
+			push @{$GROUPS{$group_name}}, $key if ($group_name);
+			$CONSTANTS{$key} = $value;
+        }
+		else {
+			die "INVALID ARGS!";
+		}
+    }
+};
 
 
 1;
